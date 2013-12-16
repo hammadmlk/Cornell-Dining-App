31f455b4afed15f710e66a1e32aa2f5853085f2c
Hanxiao <hxfu829@gmail.com>
2013-12-16 04:18:27 -0500

    Code Cleaning and configuration setting

--- js/service.js
+++ js/service.js
@@ -1,15 +1,17 @@
 // FILE TO TALK TO SERVER.
 /////////////////////////
 
-// res: 
+// res: resource          - stores information about request header, uri, etc.
 // cb: callback           - called when request succeds
 // bcb: back-up callback  - called when request fails
 function sendRequest(res, cb, bcb) {
   var TARGET_SERVER = "sf-sas-skoda01.serverfarm.cornell.edu/";
   var ROOT_DIR = location.href.substr(0,location.href.lastIndexOf('/')+1);
+  //service root points to proxy php, which is a intermediary for cross domain requests 
   var SERVICE_ROOT = ROOT_DIR +"/proxy.php?targetServer=" + TARGET_SERVER + "&url=";
 
   if ((/file/).test(window.location.protocol)) {
+    //when installed on phones, we no longer need the proxy php
     SERVICE_ROOT = "http://sf-sas-skoda01.serverfarm.cornell.edu/"
   }
 
--- proxy.php
+++ proxy.php
@@ -1,4 +1,9 @@
 <?php
+# Proxy.php
+#
+# This file is used to send cross domain requests. It is useful when testing and developing on desktop browsers
+#
+
 if (!isset($_GET['url'])) die();
 $url = urldecode($_GET['url']);
 $targetServer = $_GET['targetServer'];
@@ -6,145 +11,7 @@ $targetServer = $_GET['targetServer'];
 $debug = false;
 
 if($debug){
-//TODO...
-$pattern = '/.*\/([^.]*)./';  // gets   /abcd/ 
-if(preg_match($pattern, $url, $requested)){
-  // var_dump($requested);
-}
-
-return;
-
-echo '[
-	{
-		"campus_name": "North Campus",
-		"diners": [
-			{
-				"diner_id": 0,
-				"diner_name": "Bear Necessities",
-				"diner_desc": "- A convenience store & grill in Robert Purcell",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44787,
-					"latitude": -76.47404
-				},
-				"diner_distance": 0.3
-			},
-			{
-				"diner_id": 1,
-				"diner_name": "Carol\'s café",
-				"diner_desc": "- A café in Balch Hall",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44757,
-					"latitude": -76.47204
-				},
-				"diner_distance": 0.4
-			},
-			{
-				"diner_id": 2,
-				"diner_name": "North Star Dining Room",
-				"diner_desc": "- All Your Care To Eat dining room in Appel Commons",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44689,
-					"latitude": -76.47524
-				},
-				"diner_distance": 0.5
-			}
-		]
-	},
-	{
-		"campus_name": "West Campus",
-		"diners": [
-			{
-				"diner_id": 0,
-				"diner_name": "Bear Necessities",
-				"diner_desc": "- A convenience store & grill in Robert Purcell",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44787,
-					"latitude": -76.47404
-				},
-				"diner_distance": 0.3
-			}
-		]
-	},
-	{
-		"campus_name": "East Campus",
-		"diners": [
-			{
-				"diner_id": 0,
-				"diner_name": "Bear Necessities",
-				"diner_desc": "- A convenience store & grill in Robert Purcell",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44787,
-					"latitude": -76.47404
-				},
-				"diner_distance": 0.3
-			},
-			{
-				"diner_id": 1,
-				"diner_name": "Carol\'s café",
-				"diner_desc": "- A café in Balch Hall",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44757,
-					"latitude": -76.47204
-				},
-				"diner_distance": 0.4
-			},
-			{
-				"diner_id": 2,
-				"diner_name": "North Star Dining Room",
-				"diner_desc": "- All Your Care To Eat dining room in Appel Commons",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44689,
-					"latitude": -76.47524
-				},
-				"diner_distance": 0.5
-			}
-		]
-	},
-	{
-		"campus_name": "South Campus",
-		"diners": [
-			{
-				"diner_id": 0,
-				"diner_name": "Bear Necessities",
-				"diner_desc": "- A convenience store & grill in Robert Purcell",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44787,
-					"latitude": -76.47404
-				},
-				"diner_distance": 0.3
-			},
-			{
-				"diner_id": 1,
-				"diner_name": "Carol\'s café",
-				"diner_desc": "- A café in Balch Hall",
-				"close_at": "2013-10-14T14:00:00",
-				"is_open": 1,
-				"diner_location": {
-					"longitude": 42.44757,
-					"latitude": -76.47204
-				},
-				"diner_distance": 0.4
-			}
-		]
-	}
-]';
-
+	// TODO:// 
 }else{
   $url = $targetServer.$url;
 	$setParam = false;
@@ -162,8 +29,9 @@ echo '[
   }
   
   $url = 'http://'.$url; // Avoid accessing the file system
-  // echo $url;
+
+  // to simulate networking delay
   // sleep(rand(1,3));
+
   echo file_get_contents($url);
-  // echo preg_replace("/\/.*\ / ", "", file_get_contents($url));
 }
